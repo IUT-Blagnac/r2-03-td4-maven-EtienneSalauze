f766ee11af2300ec1760aa4223298e2cc48ae33b
EtienneSalauze <126158776+EtienneSalauze@users.noreply.github.com>
2023-02-23 11:57:49 +0100

    Add files via upload

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+package Main;
+
+public class Main {
+
+    public static void main(String argv[]) throws Exception{
+        Pile maPile = new Pile(10);
+        if (!maPile.est_vide()) System.out.println("NOK");
+
+        maPile.empiler(5);
+        if (maPile.est_vide()) System.out.println("NOK");
+
+        int element = maPile.depiler();
+        if (!maPile.est_vide()) System.out.println("NOK");
+        if (element != 5) System.out.println("NOK");
+
+    }
+}
+
+	
