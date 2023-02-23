6f7a9c53b30b68926d9d6689aa6ff42242f0785e
EtienneSalauze <michelopilol@gmail.com>
2023-02-23 11:27:03 +0000

    Ajout de la javadoc Pile.java

--- src/main/java/Pile.java
+++ src/main/java/Pile.java
@@ -8,10 +8,18 @@ public class Pile {
         this.pile = new int[pfTaille];
     }
 
+    /**
+    Donne le sommet de la pile
+    @return le sommet de la pile
+     */
     public int som() {
         return this.pile[this.som];
     }
     
+    /**
+    Dit si la pile est vide ou non
+    @return true si la pile est vide, false sinon
+     */
     public boolean est_vide() {
         if(this.som == -1)
         {
@@ -19,11 +27,19 @@ public class Pile {
         }
         return false;
     }
+    /**
+    Ajoute un élément à la pile
+    @param int pfElement, un nouvel élément de la pile
+     */
     public void empiler(int pfElement) {
         this.som++;
         this.pile[som] = pfElement;
     }
 
+    /**
+    Enlève un élément de la pile
+    @throws si la pile est déjà vide
+     */
     public int depiler() throws Exception {
         if(this.est_vide()) {
             throw new Exception("La pile est vide");
@@ -31,7 +47,10 @@ public class Pile {
         this.som--;
         return this.pile[som + 1];
     }
-        
+
+    /**
+    Vide la pile
+     */  
     public void vider(){
         this.som = -1;
     }
